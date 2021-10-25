c6dc6ccfc9eb7a963aabb510808e03fba14a8d76
alen.sljivar <alen.sljivar@five.agency>
2021-10-25 23:14:47 +0200

    add put and delete

--- index.js
+++ index.js
@@ -22,17 +22,42 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-// app.delete("/api/notes/:id", (request, response) => {
-//   const id = Number(request.params.id);
-//   notes = notes.filter((note) => note.id !== id);
-//   response.status(204).end();
-// });
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
@@ -55,6 +80,9 @@ app.post("/api/notes", (request, response) => {
 });
 
 app.use(middlewares.unknownEndpoint);
+
+// this has to be the last loaded middleware.
+app.use(middlewares.errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- src/middlewares.js
+++ src/middlewares.js
@@ -10,7 +10,18 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
+  errorHandler,
 };
