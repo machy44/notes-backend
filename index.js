23f8cde88fcfb8818b55886eeefc636483b60c58
alen.sljivar <alen.sljivar@five.agency>
2021-11-03 22:05:53 +0100

    change project file structure

--- index.js
+++ index.js
@@ -1,87 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const app = require('./app'); // the actual Express application
+const http = require('http');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-const Note = require('./src/models/note');
-const middlewares = require('./src/middlewares');
+const server = http.createServer(app);
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.static('build'));
-
-app.use(middlewares.requestLogger);
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>'); // express automatically sets the value of the Content-Type header to be text/html
-});
-
-app.get('/api/notes', (request, response) => {
-  Note.find().then((notes) => {
-    response.json(notes);
-  });
-});
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body;
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
-
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body;
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  });
-
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
-});
-
-app.use(middlewares.unknownEndpoint);
-
-// this has to be the last loaded middleware.
-app.use(middlewares.errorHandler);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
