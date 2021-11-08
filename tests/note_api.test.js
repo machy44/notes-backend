b6a8b693fc02396720fe1f18b98f567c4d411b79
alen.sljivar <alen.sljivar@five.agency>
2021-11-08 23:38:10 +0100

    done until exercises 4.8 -4.12

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -9,11 +9,33 @@ const Note = require('../models/note');
 
 beforeEach(async () => {
   await Note.deleteMany({});
-  let noteObject = new Note(helper.initialNotes[0]);
-  await noteObject.save();
-  noteObject = new Note(helper.initialNotes[1]);
-  await noteObject.save();
+  console.log('cleared');
+
+  // helper.initialNotes.forEach(async (note) => {
+  //   // The problem is that every iteration of the forEach loop generates its own asynchronous operation,
+  //   // and beforeEach won't wait for them to finish executing. In other words, the await commands
+  //   // defined inside of the forEach loop are not in the beforeEach function,
+  //   // but in separate functions that beforeEach will not wait for.
+  //   let noteObject = new Note(note);
+  //   await noteObject.save();
+  //   console.log('saved');
+  // });
+  const noteObjects = helper.initialNotes.map((note) => new Note(note));
+  // Promise.all executes the promises it receives in parallel
+  await Promise.all(noteObjects.map((note) => note.save())); //save() function creates promise
+
+  console.log('done');
 });
+
+// for of is used when you want have a specific execution order
+// beforeEach(async () => {
+//   await Note.deleteMany({});
+
+//   for (let note of helper.initialNotes) {
+//     let noteObject = new Note(note);
+//     await noteObject.save();
+//   }
+// });
 
 test('notes are returned as json', async () => {
   await api
