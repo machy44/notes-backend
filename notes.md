d43eb386d22ebe9fd13252baaa9a94e1b95549c3
alen.sljivar <alen.sljivar@five.agency>
2021-11-12 22:49:57 +0100

    finish refactoring tests

---
+++ notes.md
@@ -0,0 +1,25 @@
+```javascript
+// helper.initialNotes.forEach(async (note) => {
+  //    The problem is that every iteration of the forEach loop generates its own asynchronous operation,
+  //    and beforeEach won't wait for them to finish executing. In other words, the await commands
+  //    defined inside of the forEach loop are not in the beforeEach function,
+  //    but in separate functions that beforeEach will not wait for.
+  //   let noteObject = new Note(note);
+  //   await noteObject.save();
+  //   console.log('saved');
+  // });
+  const noteObjects = helper.initialNotes.map((note) => new Note(note));
+  // Promise.all executes the promises it receives in parallel
+  await Promise.all(noteObjects.map((note) => note.save())); //save() function creates promise
+});
+
+// for of is used when you want have a specific execution order
+// beforeEach(async () => {
+//   await Note.deleteMany({});
+
+//   for (let note of helper.initialNotes) {
+//     let noteObject = new Note(note);
+//     await noteObject.save();
+//   }
+// });
+```
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -8,112 +8,104 @@ const api = supertest(app); //for api calls
 const Note = require('../models/note');
 
 beforeEach(async () => {
-  await Note.deleteMany({});
-  console.log('cleared');
-
-  // helper.initialNotes.forEach(async (note) => {
-  //   // The problem is that every iteration of the forEach loop generates its own asynchronous operation,
-  //   // and beforeEach won't wait for them to finish executing. In other words, the await commands
-  //   // defined inside of the forEach loop are not in the beforeEach function,
-  //   // but in separate functions that beforeEach will not wait for.
-  //   let noteObject = new Note(note);
-  //   await noteObject.save();
-  //   console.log('saved');
-  // });
-  const noteObjects = helper.initialNotes.map((note) => new Note(note));
-  // Promise.all executes the promises it receives in parallel
-  await Promise.all(noteObjects.map((note) => note.save())); //save() function creates promise
-
-  console.log('done');
+  await Note.deleteMany();
+  await Note.insertMany(helper.initialNotes);
 });
 
-// for of is used when you want have a specific execution order
-// beforeEach(async () => {
-//   await Note.deleteMany({});
+describe('when there is initially some notes saved', () => {
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+  test('all notes are returned', async () => {
+    const response = await api.get('/api/notes');
 
-//   for (let note of helper.initialNotes) {
-//     let noteObject = new Note(note);
-//     await noteObject.save();
-//   }
-// });
+    expect(response.body).toHaveLength(helper.initialNotes.length);
+  });
 
-test('notes are returned as json', async () => {
-  await api
-    .get('/api/notes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+  test('a specific note is within the returned notes', async () => {
+    const response = await api.get('/api/notes');
+
+    const contents = response.body.map((r) => r.content);
+    expect(contents).toContain('Browser can execute only Javascript');
+  });
 });
 
-test('there are two notes', async () => {
-  const response = await api.get('/api/notes');
+describe('viewing a specific note', () => {
+  test('succeeds with a valid id', async () => {
+    const notestAtStart = await helper.notesInDb();
 
-  expect(response.body).toHaveLength(helper.initialNotes.length);
+    const noteToView = notestAtStart[0];
+
+    const resultNote = await api
+      .get(`/api/notes/${noteToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const processedNoteToView = JSON.parse(JSON.stringify(noteToView));
+
+    expect(resultNote.body).toEqual(processedNoteToView);
+  });
+  test('fails with status code 404 if note does not exist', async () => {
+    const validNonexistingId = await helper.nonExistingId();
+    console.log(validNonexistingId);
+    await api.get(`/api/notes/${validNonexistingId}`).expect(404);
+  });
+  test('fails with status code 400 id is invalid', async () => {
+    const invalidId = '5a3d5da59070081a82a3445';
+    await api.get(`/api/notes/${invalidId}`).expect(400);
+  });
 });
 
-test('a specific note is within the returned notes', async () => {
-  const response = await api.get('/api/notes');
+describe('addition of a new note', () => {
+  test('succeeds with valid data', async () => {
+    const newNote = {
+      content: 'async/await simplifies making async calls',
+      important: true,
+    };
 
-  const contents = response.body.map((r) => r.content);
-  expect(contents).toContain('Browser can execute only Javascript');
+    await api
+      .post('/api/notes')
+      .send(newNote)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const notesAtEnd = await helper.notesInDb();
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
+
+    const contents = notesAtEnd.map((n) => n.content);
+    expect(contents).toContain('async/await simplifies making async calls');
+  });
+  test('fails with status code 400 if data invalid', async () => {
+    const newNote = {
+      important: true,
+    };
+
+    await api.post('/api/notes').send(newNote).expect(400);
+
+    const notesAtEnd = await helper.notesInDb();
+
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
+  });
 });
 
-test('a valid note can be added', async () => {
-  const newNote = {
-    content: 'async/await simplifies making async calls',
-    important: true,
-  };
+describe('deletion of a note', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const notesAtStart = await helper.notesInDb();
+    const noteToDelete = notesAtStart[0];
 
-  await api
-    .post('/api/notes')
-    .send(newNote)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+    await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
 
-  const notesAtEnd = await helper.notesInDb();
+    const notesAtEnd = await helper.notesInDb();
 
-  expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
 
-  const contents = notesAtEnd.map((n) => n.content);
-  expect(contents).toContain('async/await simplifies making async calls');
-});
+    const contents = notesAtEnd.map((r) => r.content);
 
-test('note without content is not added', async () => {
-  const newNote = {
-    important: true,
-  };
-
-  await api.post('/api/notes').send(newNote).expect(400);
-
-  const notesAtEnd = await helper.notesInDb();
-
-  expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
-});
-
-test('a specifict note can be viewed', async () => {
-  const notestAtStart = await helper.notesInDb();
-
-  const noteToView = notestAtStart[0];
-
-  const resultNote = await api.get(`/api/notes/${noteToView.id}`);
-
-  const processedNoteToView = JSON.parse(JSON.stringify(noteToView));
-
-  expect(resultNote.body).toEqual(processedNoteToView);
-});
-
-test('a note can be deleted', async () => {
-  const notesAtStart = await helper.notesInDb();
-  const noteToDelete = notesAtStart[0];
-
-  await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
-
-  const notesAtEnd = await helper.notesInDb();
-
-  expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
-
-  const contents = notesAtEnd.map((r) => r.content);
-
-  expect(contents).not.toContain(noteToDelete.content);
+    expect(contents).not.toContain(noteToDelete.content);
+  });
 });
 
 afterAll((done) => {
